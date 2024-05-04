8e9f46810a6521e73fa1a3560d5061a46704816a
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-05 01:10:05 +0600

    create card for servies area with dynamic route assign

--- src/Components/ServiceArea/ServiceArea.jsx
+++ src/Components/ServiceArea/ServiceArea.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const ServiceArea = () => {
     const [services,setServices]=useState([]);
@@ -10,8 +11,17 @@ const ServiceArea = () => {
     },[])
 
     return (
-        <div>
-            {services.length}
+        <div className="mt-10">
+            <div className="text-center">
+                <h4 className="text-xl text-[#FF3811] font-bold">Service</h4>
+                <h2 className="text-4xl text-[#151515] font-bold">Our Service Area</h2>
+                <p className="text-sm text-[#737373]">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 mt-10 gap-4">
+                {
+                    services.map((service,idx)=><ServiceCard key={idx} service={service}></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/ServiceArea/ServiceCard.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const ServiceCard = ({ service }) => {
+  const { _id,img, title, price } = service;
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={img} alt="service_image" className="rounded-xl h-52" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-[#444444] font-bold">{title}</h2>
+          <div className="card-actions flex items-center">
+            <p className="text-[#FF3811] font-bold">Price:{price}</p>
+            <button className="btn btn-ghost text-[#FF3811] text-xl hover:bg-[#FF3811] hover:text-white hover:scale-90">
+              <Link to={`/serviceType/${_id}`}>
+                <FaArrowRightLong></FaArrowRightLong>
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ServiceCard.propTypes = {
+  service: PropTypes.object,
+};
+export default ServiceCard;
