ff5425b01d14ea9ab099dc77af51d81d9343c21d
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-04 13:06:27 +0600

    initialize the project and added route and firebase config setup also added navbar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../Shared/Header/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
