f3468616bad5ab5e965c8cd6c8850ef6d4200edb
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-20 11:47:10 +0600

    create sorting feature in service area section

--- src/Components/ServiceArea/ServiceArea.jsx
+++ src/Components/ServiceArea/ServiceArea.jsx
@@ -1,24 +1,37 @@
 import ServiceCard from "./ServiceCard";
 import useServices from "../../Hooks/useServices";
+import { useState } from "react";
 
 const ServiceArea = () => {
-    
-    const services=useServices();
+  const [sort, setSort] = useState(true);
 
-    return (
-        <div className="mt-10">
-            <div className="text-center space-y-2">
-                <h4 className="text-xl text-[#FF3811] font-bold">Service</h4>
-                <h2 className="text-4xl text-[#151515] font-bold">Our Service Area</h2>
-                <p className="text-sm text-[#737373]">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 mt-10 gap-4">
-                {
-                    services.map((service,idx)=><ServiceCard key={idx} service={service}></ServiceCard>)
-                }
-            </div>
-        </div>
-    );
+  const services = useServices(sort);
+
+  return (
+    <div className="mt-10">
+      <div className="text-center space-y-2">
+        <h4 className="text-xl text-[#FF3811] font-bold">Service</h4>
+        <h2 className="text-4xl text-[#151515] font-bold">Our Service Area</h2>
+        <p className="text-sm text-[#737373]">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which do not look even slightly
+          believable.{" "}
+        </p>
+      </div>
+      <div className="text-center my-4">
+        <button onClick={()=>setSort(!sort)} className="btn btn-warning font-mono text-white text-xl">
+          {
+            sort?"Price:High to Low":"Price:Low to High"
+          }
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-10 gap-4">
+        {services.map((service, idx) => (
+          <ServiceCard key={idx} service={service}></ServiceCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ServiceArea;
--- src/Hooks/useServices.jsx
+++ src/Hooks/useServices.jsx
@@ -1,16 +1,19 @@
-import axios from "axios";
+// import axios from "axios";
 import { useEffect, useState } from "react";
+import { axiosSecure } from "./useAxiosSecure";
 
-const useServices = () => {
+const useServices = (sort) => {
     const [services,setServices]=useState([]);
     
     useEffect(()=>{
-        axios.get("https://car-fixer-server.vercel.app/services")
-        .then(res=>{
-            setServices(res.data)
-        })
-    },[])
+        // axios.get("https://car-fixer-server.vercel.app/services")
+        // .then(res=>{
+        //     setServices(res.data)
+        // })
+        axiosSecure(`/services?sort=${sort?"asc":"des"}`)
+        .then(res => setServices(res.data))
+    },[sort])
     return services;
 };
 
-export default useServices;
+export default useServices;  
